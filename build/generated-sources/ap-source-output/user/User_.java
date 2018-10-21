7d754cf5fa54158d70cc2008d609c4e0e2599da7
AshKai <thejaimes111dev@gmail.com>
2018-10-21 11:35:27 +0000

    finish version

---
+++ build/generated-sources/ap-source-output/user/User_.java
@@ -0,0 +1,14 @@
+package user;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-05-01T10:02:24")
+@StaticMetamodel(User.class)
+public class User_ { 
+
+    public static volatile SingularAttribute<User, String> password;
+    public static volatile SingularAttribute<User, String> email;
+
+}
